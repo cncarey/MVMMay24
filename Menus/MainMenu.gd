bc62c98fcc96b8f6e62007f38c424883570bccd1
cncarey <courtneykrider@yahoo.com>
2024-05-19 13:04:31 -0400

    #11 add options to the main menu
    #11 add options to the main menu

--- Menus/MainMenu.gd
+++ Menus/MainMenu.gd
@@ -3,6 +3,8 @@ extends Control
 @onready var _continue = %Continue
 @onready var options = %Options
 @onready var quit = %Quit
+@onready var main_container = $MainContainer
+@onready var options_container = $OptionsContainer
 
 
 # Called when the node enters the scene tree for the first time.
@@ -23,6 +25,7 @@ func _process(delta):
 func _on_start_pressed():
 	Global.resetGame()
 	await LevelTransition.fadeToBlack()
+	#TODO go to the opening cut scene
 	get_tree().change_scene_to_file("res://Scenes/a_scene.tscn")
 	LevelTransition.fadeFromBlack()
 	pass # Replace with function body.
@@ -32,6 +35,14 @@ func _on_continue_pressed():
 	get_tree().change_scene_to_file(Global.currentLocation)
 	LevelTransition.fadeFromBlack()
 	pass 
+
+func hideOptions():
+	main_container.show()
+	options_container.hide()
+	
+func showOptions():
+	main_container.hide()
+	options_container.show()
 
 func _on_quit_pressed():
 	get_tree().quit()
